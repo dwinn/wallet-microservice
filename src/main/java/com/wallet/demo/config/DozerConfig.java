7b9d5e679faad7068dafa271c68dab58e13ae1da
David Winn <dwinn@turnitin.com>
2022-07-31 01:18:56 +0100

    getBalance

---
+++ src/main/java/com/wallet/demo/config/DozerConfig.java
@@ -0,0 +1,22 @@
+package com.wallet.demo.config;
+
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <description>.
+ *
+ * @author David Winn
+ */
+@Configuration
+public class DozerConfig {
+
+	@Bean
+	public Mapper mapper() {
+		return new DozerBeanMapper();
+	}
+
+}
--- src/main/java/com/wallet/demo/service/AccountService.java
+++ src/main/java/com/wallet/demo/service/AccountService.java
@@ -3,6 +3,7 @@ package com.wallet.demo.service;
 import com.wallet.demo.dto.Account;
 import com.wallet.demo.persistence.AccountEntity;
 import com.wallet.demo.persistence.AccountRepository;
+import org.dozer.Mapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,10 +22,12 @@ public class AccountService {
     private static final Logger LOGGER = LoggerFactory.getLogger(AccountService.class);
 
     private final AccountRepository accountRepository;
+    private final Mapper mapper;
 
     @Autowired
-    public AccountService(AccountRepository accountRepository) {
+    public AccountService(AccountRepository accountRepository, Mapper mapper) {
         this.accountRepository = accountRepository;
+        this.mapper = mapper;
     }
 
     public void createAccount(Account account) {
@@ -33,9 +36,14 @@ public class AccountService {
 
     public Account getBalance(int accountId) {
 
-        Optional<AccountEntity> accountEntity = accountRepository.findByAccountId(accountId);
+//        Optional<AccountEntity> accountEntity = accountRepository.findByAccountId(accountId);
+//
+//        return accountEntity.map(entity -> new Account(accountEntity.get())).orElse(null);
 
-        return accountEntity.map(entity -> new Account(accountEntity.get())).orElse(null);
+
+
+        AccountEntity account = accountRepository.findByAccountId(accountId).orElse(null);
+        return mapper.map(account, Account.class);
 
         // TODO: Change the ELSE to a more friendly output. Maybe an error message.
     }
